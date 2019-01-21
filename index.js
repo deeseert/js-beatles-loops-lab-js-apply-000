60cd2ef28fc78a38275768ba00eaf1a749dea63f
Gioacchino Di Nardo <gio.amms@gmail.com>
2019-01-21 08:53:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(mu, ins){
     let emptyArr = [];
     for (var i = 0; i < mu.length; i++) {
-      emptyArr.push(mu[i] + " plays " + ins[i]);
+      emptyArr.push(`${mu[i]} plays ${ins[i]}`);
     }
     return emptyArr;
 }
