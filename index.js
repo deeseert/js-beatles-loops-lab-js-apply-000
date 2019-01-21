14719d21833d062a63fac84e12339c85fd6d8091
Gioacchino Di Nardo <gio.amms@gmail.com>
2019-01-21 08:52:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-function theBeatlesPlay(mu, instruments){
+function theBeatlesPlay(mu, ins){
     let emptyArr = [];
     for (var i = 0; i < mu.length; i++) {
-      emptyArr.push(mu[i] + " plays " instruments[i]);
+      emptyArr.push(mu[i] + " plays " + ins[i]);
     }
     return emptyArr;
 }
